6d4a48e66f25a68deb610251d77e81f5c60c09c5
Aura-Guardian <auraguardian0228@gmail.com>
2024-04-03 08:58:21 +0530

    New practicals

---
+++ INS/P8/RNG.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+vector<int> linear_congruential(int a, int c, int m, int Xo, int n){
+    vector<int> random_number; 
+    random_number[0] = Xo;
+    for(int i = 1; i < n; i++){ 
+        random_number[i] = ((a * random_number[i-1]) + c) % m;
+    }
+    return random_number;
+}
+
+
+int main(){
+    int a, c, m, Xo1;
+    int p, q, n, Xo2;
+    int no;
+    ifstream f1("bbs_paramters.txt");
+    ifstream f2("lc_paramters.txt");
+    f1 >> a >> c >> m >> Xo1;
+    f2 >> p >> q >> Xo2;
+    cout<<"Enter number of random numbers to generate: "<<endl;
+    cin>>no;
+    vector<int> nos1 = linear_congruential(a, c, m, Xo1, no);
+    for(int i = 0; i < nos1.size(); i++){
+        cout<<nos1[i]<<endl;
+    }
+}
